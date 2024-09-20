4b0abfadfabb997dce4ed14f2e36947104ef56a2
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-09-20 17:34:07 -0500

    using tanstack query and axios

---
+++ src/Repositories.jsx
@@ -0,0 +1,38 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+export default function Repositories() {
+  const getRepositories = async () => {
+    const response = await axios.get(
+      "https://api.github.com/search/repositories?q=react"
+    );
+    return response.data.items;
+  };
+
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ["repositories"],
+    queryFn: getRepositories,
+  });
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (isError) {
+    return <p>Error...</p>;
+  }
+
+  return (
+    <table>
+      <tbody>
+        {data.map((repo) => (
+          <tr key={repo.id}>
+            <td>{repo.full_name}</td>
+            <td>
+              <a href={repo.html_url}>{repo.html_url}</a>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
